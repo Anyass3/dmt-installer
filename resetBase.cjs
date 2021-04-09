b9b63f93237f575770de3aa4da7552efd111d95c
Anyass3 <nyassabu@gmail.com>
2021-04-09 17:52:09 +0000

    improved

--- resetBase.cjs
+++ resetBase.cjs
@@ -1,12 +1,17 @@
 const svelteConfig = 'svelte.config.cjs';
 
-const AddedConfig = /kit\:[\ ]{[\t\n\ ]*paths\:[\ ]{[\t\n\ ]*assets:[\ ]*\'\/youtube\-dl\'\,[\t\n\ ]*base:[\ ]*\'\/youtube\-dl\'[\t\n\ ]*\}[\ ]*\,/;
-
 const fs = require('fs');
+
+let settings = fs.readFileSync('dmt/settings.json', 'utf-8');
+settings = JSON.parse(settings);
+
+const app_base = settings.app_base;
+
+const AddedConfig = `[\\t\\n\\ ]*kit\\:[\\ ]{[\\t\\n\\ ]*paths\\:[\\ ]{[\\t\\n\\ ]*assets:[\\ ]*\\'\\/${app_base}\\'\\,[\\t\\n\\ ]*base:[\\ ]*\\'\\/${app_base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,`;
 
 if (fs.existsSync(svelteConfig)) {
   svelteConfigFile = fs.readFileSync(svelteConfig, 'utf8');
-  svelteConfigFile = svelteConfigFile.replace(AddedConfig, `kit: {`);
+  svelteConfigFile = svelteConfigFile.replace(RegExp(AddedConfig), `\n\tkit: {`);
   fs.writeFileSync(svelteConfig, svelteConfigFile);
 
   console.log(`restored svelte.config.cjs`);
