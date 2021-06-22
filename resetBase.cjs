71a6b26ac2cbc90f146466e1f1a3c3ba4a275af7
Anyass3 <nyassabu@gmail.com>
2021-06-22 15:21:51 +0000

    updated to support latest svelte-kit

--- resetBase.cjs
+++ resetBase.cjs
@@ -1,18 +1,22 @@
-const svelteConfig = 'svelte.config.cjs';
-
+const { join: pathJoin } = require('path');
 const fs = require('fs');
 
 let settings = fs.readFileSync('dmt/settings.json', 'utf-8');
 settings = JSON.parse(settings);
 
-const app_base = settings.app_base;
+const app_base = pathJoin('/', settings.app_base);
 
 const AddedConfig = `[\\t\\n\\ ]*paths\\:[\\ ]{[\\t\\n\\ ]*assets:[\\ ]*\\'${app_base}\\'\\,[\\t\\n\\ ]*base:[\\ ]*\\'${app_base}\\'[\\t\\n\\ ]*\\}[\\ ]*\\,[\\t\\n\\ ]*`;
 
-if (fs.existsSync(svelteConfig)) {
-  svelteConfigFile = fs.readFileSync(svelteConfig, 'utf8');
-  svelteConfigFile = svelteConfigFile.replace(RegExp(AddedConfig), `\n\t`);
-  fs.writeFileSync(svelteConfig, svelteConfigFile);
+const restore = (filePath) => {
+  if (fs.existsSync(filePath)) {
+    svelteConfigFile = fs.readFileSync(filePath, 'utf8');
+    svelteConfigFile = svelteConfigFile.replace(RegExp(AddedConfig), `\n\t`);
+    fs.writeFileSync(filePath, svelteConfigFile);
 
-  console.log(`restored svelte.config.cjs`);
-}
+    console.log(`restored svelte.config.cjs`);
+  }
+};
+
+restore('svelte.config.js');
+restore('svelte.config.cjs');
